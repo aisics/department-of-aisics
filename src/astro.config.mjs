170aebeafbf5838f3cac460adef85c922cac64e1
Mykhaylo Katruk <mykhaylo.katruk@uipath.com>
2025-07-24 13:31:32 +0300

    fix: matrix article

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,33 +2,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeKatex from 'rehype-katex';
 import { rehypeCopyCode } from './src/utils/rehype-copy-code.js';
 
 export default defineConfig({
   output: 'static',
   integrations: [
-    tailwind(),
-    mdx({
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [
-        rehypeSlug,
-        [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-        rehypeCopyCode
-      ],
-      shikiConfig: {
-        theme: 'github-light',
-        wrap: true,
-        transformers: []
-      }
-    })
+    tailwind()
   ],
   markdown: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkGfm, remarkMath],
     rehypePlugins: [
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+      rehypeKatex,
       rehypeCopyCode
     ],
     shikiConfig: {
